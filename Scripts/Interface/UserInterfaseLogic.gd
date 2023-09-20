2205a6558125256abc50e5139a274d5b599e7aef
SerJolion <roseamongmanure@gmail.com>
2023-09-20 13:18:01 +0300

    Добавил сообщения
    Добавил возможность показывать текстовые сообщения во время игры

--- Scripts/Interface/UserInterfaseLogic.gd
+++ Scripts/Interface/UserInterfaseLogic.gd
@@ -7,15 +7,26 @@ extends Control
 @onready var FuelBar:ProgressBar = $MarginContainer/VBoxContainer/BarsContainer/FuelContainer/FuelBar
 @onready var EffectIconContainer:HBoxContainer = $MarginContainer/VBoxContainer/EffectsIconContainer
 @onready var PauseMEnuPanel:Panel = $MarginContainer/VBoxContainer/Main/PauseMenuPanel
+@onready var MessagePanel:Panel = $MarginContainer/VBoxContainer/Main/MesagePanel
+@onready var MessageTextLabel:RichTextLabel = $MarginContainer/VBoxContainer/Main/MesagePanel/MarginContainer/VBoxContainer/ScrollContainer/RichTextLabel
+
+var PauseOpened:bool = false
+var MessageOpened:bool = false
 
 var ActiveEffectIcons:Dictionary = {}
 
 func _ready():
 	PauseMEnuPanel.hide()
+	MessagePanel.hide()
+	ShowMessagePanel('Тестовое сообщение')
 
 func _process(delta):
 	if Input.is_action_just_pressed('ui_cancel'):
-		ShowPauseMenu()
+		if not MessageOpened:
+			if PauseOpened:
+				ClosePauseMenu()
+			else:
+				ShowPauseMenu()
 
 func Init(player:Node2D)->void:
 	player.HealthChanged.connect(UpdateHealthBar)
@@ -38,13 +49,26 @@ func ShowEndGamePanel(Title:String, TitleColor:Color):
 	add_child(EndGamePanel)
 	EndGamePanel.SetTitle(Title, TitleColor)
 
+func ShowMessagePanel(Message:String)->void:
+	Global.Pause(true)
+	MessagePanel.show()
+	MessageTextLabel.text = Message
+	MessageOpened = true
+
+func CloseMessagePanel()->void:
+	Global.Pause(false)
+	MessagePanel.hide()
+	MessageOpened = false
+
 func ShowPauseMenu()->void:
 	Global.Pause(true)
 	PauseMEnuPanel.show()
+	PauseOpened = true
 
 func ClosePauseMenu()->void:
 	Global.Pause(false)
 	PauseMEnuPanel.hide()
+	PauseOpened = false
 
 func AddActiveEffectIcon(effect:Effect):
 	if effect.VisibleIcon:
@@ -69,3 +93,6 @@ func _on_main_menu_button_pressed():
 
 func _on_exit_button_pressed():
 	Global.Exit()
+
+func _on_message_ok_button_pressed():
+	CloseMessagePanel()
