8ae97700438e0ddd04ddda0f3f8a515227a6c221
SerJolion <roseamongmanure@gmail.com>
2023-09-21 12:21:52 +0300

    Добавил классы эффектов
    Добавил несколько классов для новых эффектов игрока

---
+++ Scripts/Effects/FireDamageUpEffect.gd
@@ -0,0 +1,28 @@
+extends Effect
+
+class_name FireDamageUpEffect
+
+@export var Value:float = 0
+#@export_multiline var ProcessExpresion:String = ''
+
+func ExecuteValue(expression:String, VarNames:Array, VarValues:Array):
+	var exp:Expression = Expression.new()
+	var Error = exp.parse(expression, VarNames)
+	if Error != OK:
+		push_error(exp.get_error_text())
+		return null
+	return exp.execute(VarValues)
+
+func Start(Entity, World):
+#	var FinishValue = ExecuteValue(ProcessExpresion, ['Speed'], [Entity.Speed])
+#	if FinishValue != null:
+#		Entity.Speed = FinishValue	
+	if 'FireDamage' in Entity:
+		Entity.FireDamage += Value
+
+func End(Entity, World):
+#	var FinishValue = ExecuteValue(ProcessExpresion, ['Speed'], [Entity.Speed])
+#	if FinishValue != null:
+#		Entity.Speed -= FinishValue
+	if 'FireDamage' in Entity:
+		Entity.FireDamage -= Value
---
+++ Scripts/Effects/SpeedDownEffect.gd
@@ -0,0 +1,26 @@
+extends Effect
+
+class_name SpeedDownEffect
+
+@export var Value:float = 0
+#@export_multiline var ProcessExpresion:String = ''
+
+func ExecuteValue(expression:String, VarNames:Array, VarValues:Array):
+	var exp:Expression = Expression.new()
+	var Error = exp.parse(expression, VarNames)
+	if Error != OK:
+		push_error(exp.get_error_text())
+		return null
+	return exp.execute(VarValues)
+
+func Start(Entity, World):
+#	var FinishValue = ExecuteValue(ProcessExpresion, ['Speed'], [Entity.Speed])
+#	if FinishValue != null:
+#		Entity.Speed = FinishValue
+	Entity.Speed -= Value
+
+func End(Entity, World):
+#	var FinishValue = ExecuteValue(ProcessExpresion, ['Speed'], [Entity.Speed])
+#	if FinishValue != null:
+#		Entity.Speed -= FinishValue
+	Entity.Speed += Value
