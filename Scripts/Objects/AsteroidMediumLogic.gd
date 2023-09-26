007daa4c1f509abc954d9a642e2fad3836799899
SerJolion <129949115+SerJolion@users.noreply.github.com>
2023-09-26 18:50:53 +0300

    Полировка
    Теперь корабли врагов наносят урон при столкновении, небольшие правки интерфейса, увеличил шанс спавна топлива из средних астероидов

--- Scripts/Objects/AsteroidMediumLogic.gd
+++ Scripts/Objects/AsteroidMediumLogic.gd
@@ -3,7 +3,7 @@ extends "res://Scripts/Objects/AsteroidLogic.gd"
 func Destroy():
 	randomize()
 	var Chance = randf()
-	if Chance <= 0.3:
+	if Chance <= 0.5:
 		var Loot:Node2D = load("res://Objects/Powerups/FuelPowerUp.tscn").instantiate()
 		get_parent().Add2DObject(Loot, global_position)
 	get_parent().AddParticlesObject(30, true, 1.5, true, DestroyParticlesMaterial, position, Visual.color)
